4ae91c54ecb5cd964cd2d52a8ef5ecd0b5901ab8
Jxse0 <jm.expositojavier@gmail.com>
2024-03-06 17:34:23 +0100

    follow and unfollow user

--- backend/src/user/controller.ts
+++ backend/src/user/controller.ts
@@ -72,6 +72,82 @@ const controller = {
       }
     }
   },
+
+  async followUser(request: Request, response: Response, next: NextFunction) {
+    try {
+      const user_token = getTokenParameter(request);
+      const username2follow = request.body.username2follow;
+      const user = await service.followUser(user_token.userid, username2follow);
+
+      response.status(201).send({
+        status: "success",
+        data: user,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        next(error);
+      } else {
+        next(new Error("An unknown error occurred"));
+      }
+    }
+  },
+
+  async unfollowUser(request: Request, response: Response, next: NextFunction) {
+    try {
+      const user_token = getTokenParameter(request);
+      const username2unfollow = request.body.username2unfollow;
+      const user = await service.unfollowUser(
+        user_token.userid,
+        username2unfollow
+      );
+
+      response.status(201).send({
+        status: "success",
+        data: user,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        next(error);
+      } else {
+        next(new Error("An unknown error occurred"));
+      }
+    }
+  },
+  async getFollowers(request: Request, response: Response, next: NextFunction) {
+    try {
+      const user_token = getTokenParameter(request);
+      const user = await service.getFollowers(user_token.userid);
+
+      response.status(201).send({
+        status: "success",
+        data: user,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        next(error);
+      } else {
+        next(new Error("An unknown error occurred"));
+      }
+    }
+  },
+
+  async getFollowing(request: Request, response: Response, next: NextFunction) {
+    try {
+      const user_token = getTokenParameter(request);
+      const user = await service.getFollowing(user_token.userid);
+
+      response.status(201).send({
+        status: "success",
+        data: user,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        next(error);
+      } else {
+        next(new Error("An unknown error occurred"));
+      }
+    }
+  },
 };
 
 export default controller;
